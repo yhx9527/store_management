bcd2de54b432aff6f083bcbc61757bd9000ebfde
yhx9527 <540230904@qq.com>
2019-02-15 11:46:20 +0800

    配置打包去掉console

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,20 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 module.exports = {
     configureWebpack: {
-      devtool: 'source-map'
+      devtool: 'source-map',
+      optimization: {
+        minimizer: [
+          new UglifyJsPlugin({
+            uglifyOptions: {
+              compress: {
+                warnings: false,
+                drop_console: true,//console
+                drop_debugger: false,
+                pure_funcs: ['console.log']//移除console
+              }
+            }
+        })
+      ]
+      }
     }
   }
